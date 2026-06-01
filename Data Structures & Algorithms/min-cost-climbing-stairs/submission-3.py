61ff450d06253431be6e85fb0377c7909a71cdf9
Ivan Raytchinov <112513320+lucatotem@users.noreply.github.com>
2026-06-01 13:40:55 +0100

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @lru_cache(None)
+        def dp(n):
+            if n < 0:
+                return 0
+            else:
+                old = dp(n-2)
+                new = dp(n-1)
+                return min(old,new)+cost[n] if len(cost) > n else min(old,new)
+        return dp(len(cost))
+        
+
+        
