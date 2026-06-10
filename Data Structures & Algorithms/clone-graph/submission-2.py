8f99095b1f18fea98921cef20bda05ba6326ae8a
Ivan Raytchinov <112513320+lucatotem@users.noreply.github.com>
2026-06-10 11:40:44 +0100

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        dq = deque([node])
+        res = Node(node.val)
+        mp = {node:res}
+        while dq:
+            temp = dq.popleft()
+            temp_copy = mp[temp]
+            for n in temp.neighbors:
+                if not (n in mp):
+                    n_copy = Node(n.val)
+                    mp[n]=n_copy
+                    dq.append(n)
+                else:
+                    n_copy = mp[n]
+                temp_copy.neighbors.append(n_copy)
+        return res
+
+            
