7cab570478a7dc3d35ceead1ca327b5816f59e90
Ivan Raytchinov <112513320+lucatotem@users.noreply.github.com>
2026-06-10 12:05:06 +0100

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        dq = deque([])
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == 0:
+                    dq.append((x,y,0))
+        while dq:
+            tx, ty, dist = dq.popleft()
+            if grid[ty][tx] != -1 and grid[ty][tx] >= dist:
+                grid[ty][tx] = dist
+                for drx,dry in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    newx, newy = tx+drx, ty+dry
+                    if 0<=newx<len(grid[0]) and 0<=newy<len(grid):
+                        dq.append((newx,newy,dist+1))
