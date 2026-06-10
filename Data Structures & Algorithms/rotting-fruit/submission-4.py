0de01b6c3585e57c8f026921a7f4f846f9307aaa
Ivan Raytchinov <112513320+lucatotem@users.noreply.github.com>
2026-06-10 12:34:09 +0100

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        dq = deque([])
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == 2:
+                    dq.append((x,y))
+        curMax = 0
+        while dq:
+            directions = [(1,0),(-1,0),(0,1),(0,-1)]
+            x,y = dq.popleft()
+            for dx,dy in directions:
+                nx,ny = x+dx,y+dy
+                if 0<=nx<len(grid[0]) and 0<=ny<len(grid) and grid[ny][nx] == 1:
+                    grid[ny][nx] = grid[y][x]+1
+                    dq.append((nx,ny))
+                    curMax = max(curMax,grid[ny][nx]-2)
+                    
+
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == 1:
+                    return -1
+        return curMax
