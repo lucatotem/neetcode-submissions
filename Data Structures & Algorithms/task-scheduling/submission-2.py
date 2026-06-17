0ba12a4210c1492cf334161e78105b6f5810f6ed
Ivan Raytchinov <112513320+lucatotem@users.noreply.github.com>
2026-06-17 11:10:57 +0100

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,22 @@
+from collections import Counter,deque
+import heapq
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        frequencyList = list(Counter(tasks).values())
+        dq = deque([])
+
+        heapq.heapify_max(frequencyList)
+        time = 0
+        while frequencyList or dq:
+            if dq and dq[0][1] == time:
+                temp_val,temp_time = dq.popleft()
+                heapq.heappush_max(frequencyList,temp_val)
+            if frequencyList:
+                temp = heapq.heappop_max(frequencyList)
+                if temp != 1:
+                    dq.append((temp-1,time+n+1))
+            time+=1
+        return time
+
+            
+                
